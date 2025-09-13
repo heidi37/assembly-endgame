49e2ab99687594157bab8a103e79935f36737447
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-09-13 14:55:01 -0400

    [TASK] Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Assembly: Endgame
+
+Scrimba React Project
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe from Assembly!
+        </p>
+      </header>
+      
+  
+    </main>
+  )
+}
+
+export default App
