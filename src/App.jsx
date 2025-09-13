593a952967066659e8e8c4f9a1df57b51bd8b7bb
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-09-13 18:44:03 -0400

    [TASK] Add styling to winning status block

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-import './App.css'
+import "./App.css"
 
 function App() {
-
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>
-          Guess the word in under 8 attempts to keep the programming world safe from Assembly!
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
         </p>
       </header>
-      
-  
+      <section className="status won">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
     </main>
   )
 }
