eff0ea1f9368614904e921b616d6170930bdde0e
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-09-14 12:04:37 -0400

    [TASK] style game interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react"
-
 import "./App.css"
+import { languages } from "./languages"
 
 function App() {
+  const [currentWord, setCurrentWord] = useState('test')
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  const keys = alphabet.split("").map(key => (
+    <button className="key-buttons" key={key}>{key.toUpperCase()}</button>
+  ))
+
+  const languageEls = languages.map((language) => (
+    <span className="chip"
+      key={language.name}
+      style={{
+        backgroundColor: language.backgroundColor,
+        color: language.color,
+      }}
+    >
+      {language.name}
+    </span>
+  ))
+
+  const wordLetters = currentWord.split("").map((letter, index) => (
+    <span className="word-letters" key={index}>{letter.toUpperCase()}</span>
+  ))
   return (
     <main>
       <header>
@@ -16,6 +39,10 @@ function App() {
         <h2>You win!</h2>
         <p>Well done! 🎉</p>
       </section>
+      <section className="chips">{languageEls}</section>
+      <section className="word">{wordLetters}</section>
+      <section className="keyboard">{keys}</section>
+      <button className="new-game">New Game</button>
     </main>
   )
 }
